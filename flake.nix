af2acb3ab1d34ea2299f0eb5c68f90b7a34e4c9b
Sylvain Carisey <sylvain@carisey.dev>
2026-03-04 09:38:44 +0100

    ci(update):fix pipeline

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    devenv.url = "github:cachix/devenv/v1.11.1";
   };
 
   outputs =
@@ -16,6 +17,7 @@
       nixpkgs,
       flake-utils,
       rust-overlay,
+      devenv,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
@@ -40,13 +42,13 @@
         packages = {
           inherit pullix;
           default = pullix;
+          devenv = devenv.packages."${system}".default;
         };
 
         formatter = pkgs.alejandra;
       }
     )
     // {
-      # NixOS module for running pullix as a service
       nixosModules.default = import ./nix/nixos-module.nix { inherit self; };
     };
 
@@ -54,10 +56,14 @@
     extra-substituters = [
       "https://nix-community.cachix.org?priority=31"
       "https://scarisey-public.cachix.org?priority=32"
+      "https://devenv.cachix.org?priority=33"
+      "https://cachix.cachix.org?priority=34"
     ];
     extra-trusted-public-keys = [
       "scarisey-public.cachix.org-1:kabqlCM0Wwd3iOh+C62WQg7vUO7vX3JbKKraSmxr2n8="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
     ];
   };
 }
