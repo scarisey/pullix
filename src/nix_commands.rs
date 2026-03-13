0dab62010bb945b88f572b2d5457756c3eeb1d3b
Sylvain Carisey <sylvain@carisey.dev>
2026-03-13 14:30:49 +0100

    feat:write error reports

--- src/nix_commands.rs
+++ src/nix_commands.rs
@@ -1,13 +1,69 @@
-use crate::{flake::FlakeRef, nix_commands};
-use anyhow::{Context, Result, anyhow};
+use std::fmt::Display;
+
+use crate::{config::Config, flake::FlakeRef, nix_commands};
+use anyhow::Result;
+use serde::Serialize;
+use thiserror::Error;
 use tokio::process::Command;
 use tracing::debug;
 
-pub trait NixCommands {
-    async fn deploy(&self, flake_ref: &FlakeRef, hostname: &str) -> Result<()>;
+#[derive(Error, Debug, Serialize)]
+pub enum NixCommandError {
+    #[error("Build failed: {hostname}\nstdout: {stdout}\nstderr: {stderr}")]
+    Build {
+        hostname: String,
+        stderr: String,
+        stdout: String,
+    },
+    #[error("Execution failed: {message}")]
+    Execution { message: String },
+    #[error("Switch failed: {hostname}\nstdout: {stdout}\nstderr: {stderr}")]
+    Switch {
+        hostname: String,
+        stderr: String,
+        stdout: String,
+    },
 }
-async fn deploy(flake_ref: &FlakeRef, hostname: &str, switch_args: &[&str]) -> Result<()> {
-    let flake_url = flake_ref.to_flake_url()?;
+impl NixCommandError {
+    pub fn to_execution(error: impl Into<Box<dyn std::error::Error>> + Display) -> Self {
+        Self::Execution {
+            message: error.to_string(),
+        }
+    }
+    pub async fn report_error(&self, config: &Config) -> Result<()> {
+        let serialized = serde_json::to_string_pretty(self)?;
+        let last_error_path = Self::last_error_path(config);
+        if last_error_path.exists() {
+            let error_path = Self::error_path(config);
+            tokio::fs::rename(&last_error_path, error_path).await?;
+        }
+        tokio::fs::write(&last_error_path, serialized).await?;
+        Ok(())
+    }
+
+    fn last_error_path(config: &Config) -> std::path::PathBuf {
+        format!("{}/last_report.json", config.app_dir).into()
+    }
+    fn error_path(config: &Config) -> std::path::PathBuf {
+        let ts = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        format!("{}/{}_report.json", config.app_dir, ts,).into()
+    }
+}
+
+pub trait NixCommands {
+    async fn deploy(&self, flake_ref: &FlakeRef, hostname: &str) -> Result<(), NixCommandError>;
+}
+async fn deploy(
+    flake_ref: &FlakeRef,
+    hostname: &str,
+    switch_args: &[&str],
+) -> Result<(), NixCommandError> {
+    let flake_url = flake_ref
+        .to_flake_url()
+        .map_err(NixCommandError::to_execution)?;
     debug!(
         "Starting {} deployment from {}",
         switch_args.join(" "),
@@ -25,17 +81,16 @@ async fn deploy(flake_ref: &FlakeRef, hostname: &str, switch_args: &[&str]) -> R
         ])
         .output()
         .await
-        .context("Failed to execute nix build")?;
+        .map_err(NixCommandError::to_execution)?;
 
     if !build_output.status.success() {
         let stderr = String::from_utf8_lossy(&build_output.stderr);
         let stdout = String::from_utf8_lossy(&build_output.stdout);
-        return Err(anyhow!(
-            "Building {} failed:\nstdout: {}\nstderr: {}",
-            hostname,
-            stdout,
-            stderr
-        ));
+        return Err(NixCommandError::Build {
+            hostname: hostname.into(),
+            stderr: stderr.into(),
+            stdout: stdout.into(),
+        });
     }
 
     debug!("Nix build completed successfully, parsing output...");
@@ -43,23 +98,25 @@ async fn deploy(flake_ref: &FlakeRef, hostname: &str, switch_args: &[&str]) -> R
     let derivation_path = output_parsed
         .lines()
         .rfind(|line| line.starts_with("/nix/store/"))
-        .ok_or_else(|| anyhow!("Failed to find build output path in nix build output"))?;
+        .ok_or_else(|| NixCommandError::Execution {
+            message: "No derivation path found in build output".into(),
+        })?;
 
     debug!("Built derivation at {}", derivation_path);
     let output = Command::new(format!("{}/bin/switch-to-configuration", derivation_path))
         .args(switch_args)
         .output()
         .await
-        .context("Failed to execute switch-to-configuration")?;
+        .map_err(NixCommandError::to_execution)?;
 
     if !output.status.success() {
         let stderr = String::from_utf8_lossy(&output.stderr);
         let stdout = String::from_utf8_lossy(&output.stdout);
-        return Err(anyhow!(
-            "switch-to-configuration failed:\nstdout: {}\nstderr: {}",
-            stdout,
-            stderr
-        ));
+        return Err(NixCommandError::Switch {
+            hostname: hostname.into(),
+            stderr: stderr.into(),
+            stdout: stdout.into(),
+        });
     }
 
     debug!("Deployment completed successfully");
@@ -68,13 +125,13 @@ async fn deploy(flake_ref: &FlakeRef, hostname: &str, switch_args: &[&str]) -> R
 
 pub struct Prod;
 impl NixCommands for Prod {
-    async fn deploy(&self, flake_ref: &FlakeRef, hostname: &str) -> Result<()> {
+    async fn deploy(&self, flake_ref: &FlakeRef, hostname: &str) -> Result<(), NixCommandError> {
         nix_commands::deploy(flake_ref, hostname, &["switch"]).await
     }
 }
 pub struct Test;
 impl NixCommands for Test {
-    async fn deploy(&self, flake_ref: &FlakeRef, hostname: &str) -> Result<()> {
+    async fn deploy(&self, flake_ref: &FlakeRef, hostname: &str) -> Result<(), NixCommandError> {
         nix_commands::deploy(flake_ref, hostname, &["test"]).await
     }
 }
