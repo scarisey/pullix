2558606b7399033b6bde836322f1000abc9814f5
Sylvain Carisey <sylvain@carisey.dev>
2026-05-01 09:56:40 +0200

    feat: otlp traces

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,37 @@ use pullix::{
     *,
 };
 use tracing::{Level, debug, span};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    let config_path = std::env::var("PULLIX_CONFIG").context("Can't find PULLIX_CONFIG env var")?;
+    let config = Config::load_from_path(&config_path)
+        .with_context(|| format!("Failed to load config from {}", config_path))?;
+
+    let meter_provider = setup_otel(&config);
+
+    if config.otel_http_endpoint.is_some() {
+        tracing_subscriber::registry()
+            .with(
+                tracing_subscriber::fmt::layer()
+                    .with_target(true)
+                    .with_thread_ids(true)
+                    .with_file(true)
+                    .with_line_number(true),
+            )
+            .with(tracing_opentelemetry::layer())
+            .init();
+    } else {
+        tracing_subscriber::fmt::init();
+    }
+
     let root = span!(Level::TRACE, "pullix_start");
     let _ = root.enter();
     debug!("Pullix starting...");
 
-    let config_path = std::env::var("PULLIX_CONFIG").context("Can't find PULLIX_CONFIG env var")?;
-    let config = Config::load_from_path(&config_path)
-        .with_context(|| format!("Failed to load config from {}", config_path))?;
     let git = Git::new()?;
 
-    let meter_provider = setup_otel(&config);
     let meter = meter_provider
         .as_ref()
         .map(|meter_provider| meter_provider.meter("pullix"))
