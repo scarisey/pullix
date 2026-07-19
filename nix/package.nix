48c40ef72dba1fd593072936bd985922c4d7ccc6
Sylvain Carisey <sylvain@carisey.dev>
2026-07-19 10:32:38 +0200

    ci: fix nix build, missing certs

--- nix/package.nix
+++ nix/package.nix
@@ -10,6 +10,7 @@
     libgit2
     libssh2
     zlib
+    cacert
   ];
 
   nativeBuildInputs = with pkgs; [
@@ -31,6 +32,7 @@ in
     # Set environment variables for native dependencies
     OPENSSL_NO_VENDOR = 1;
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
 
     meta = with pkgs.lib; {
       description = "NixOS deployment automation tool";
